869aa48fad07b436babb94219527b9f537743aed
Dierk Koenig <dierk.koenig@canoo.com>
2018-05-27 23:20:11 +0200

    before week 12

--- dataflow/dataflowTest.js
+++ dataflow/dataflowTest.js
@@ -1,32 +1,53 @@
 // requires dataflow.js
 
-// test("dataflow-value", assert => {
-//
-//     const z = DataFlowVariable(() => x() + y());   // z depends on x and y, which are set later...
-//     const x = DataFlowVariable(() => y());         // x depends on y, which is set later...
-//     const y = DataFlowVariable(() => 1);
-//
-//     assert.is(z(), 2);
-//     assert.is(x(), 1);
-//     assert.is(y(), 1);
-//
-// });
-//
-// test("dataflow-cache", assert => { // value must be set at most once
-//
-//     let counter = 0;
-//     const x = DataFlowVariable(() => {
-//         counter++;
-//         return 1;
-//     });
-//
-//     assert.is(counter, 0);
-//     assert.is(x(), 1);
-//     assert.is(counter, 1);
-//     assert.is(x(), 1);
-//     assert.is(counter, 1);
-//
-// });
+
+test("dataflow-value", assert => {
+
+    const z = DataFlowVariable(() => x() + y());    // z depends on x and y, which are set later...
+    const x = DataFlowVariable(() => y());         // x depends on y, which is set later...
+    const y = DataFlowVariable(() => 1);
+
+    assert.is(z(), 2);
+    assert.is(x(), 1);
+    assert.is(y(), 1);
+
+});
+
+test("dataflow-cache", assert => { // value must be set at most once
+
+    let counter = 0;
+    const x = DataFlowVariable(() => {
+        counter++;
+        return 1;
+    });
+
+    assert.is(counter, 0);
+    assert.is(x(), 1);
+    assert.is(counter, 1);
+    assert.is(x(), 1);
+    assert.is(counter, 1);
+
+});
+
+
+test("dataflow-async", assert => { // promise must be set at most once
+
+    let counter = 0;
+
+    const x = DataFlowVariable( async () => await y() * 3);
+    const y = DataFlowVariable(() => {
+        counter++;
+        return new Promise( ok => setTimeout(ok(3), 10))
+    });
+
+    x().then( val => assert.is(counter,1));
+    x().then( val => assert.is(val,9));
+    x().then( val => assert.is(counter,1)); // yes, again!
+    assert.true(true) // check console
+
+});
+
+
 
 test("dataflow-scheduler", assert => {
 
