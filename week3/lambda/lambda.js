812b92fbf29b236e808fc7aa2e138c28ac251863
Dierk Koenig <dierk.koenig@canoo.com>
2018-10-03 10:22:14 +0200

    after week 3

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -15,3 +15,17 @@ const C  = flip;
 // const KI = flip (K);
 
 const T  = konst;
+// const F  =  KI;
+// const F  =  flip(K);
+const F  =  flip(T);
+
+const not = flip ;
+
+const and = p => q => p (q) (p);
+// const and = p => flip (p) (p);
+
+const pair = x => y => f => f(x)(y);
+
+const fst = p => p (T);
+const snd = p => p (F);
+
--- week3/lambda/lambdaTest.js
+++ week3/lambda/lambdaTest.js
@@ -24,19 +24,32 @@
 
 // true
 
-    ok.push (  T (1) (0) === 1 );
+    ok.push ( T (1) (0) === 1 );
 
 // false
 
+    ok.push( F (1) (0) === 0  );
+
 // not
 
+    ok.push( not(T)(1)(0) === 0);
+    ok.push( not(F)(1)(0) === 1);
+
 // and
+
+    ok.push( and (T) (T) === T );
+    ok.push( and (T) (F) === F );
+    ok.push( and (F) (T) === F );
+    ok.push( and (F) (F) === F );
 
 // or
 
 // beq
 
 // pair, V
+
+    ok.push(  fst ( pair (1) (0) ) === 1  );
+    ok.push(  snd ( pair (1) (0) ) === 0  );
 
 // pair equal
 
