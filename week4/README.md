64c3da5f364db1e73d0d2f8d501728b1612b25f2
Dierk Koenig <dierk.koenig@canoo.com>
2018-10-16 18:04:35 +0200

    before week 4

---
+++ week4/README.md
@@ -0,0 +1,3 @@
+Student Matr.Nr: 
+
+# WebProgramming
---
+++ week4/excel/excelTest.js
@@ -0,0 +1,14 @@
+// requires excel.js
+
+// todo: enable tests
+
+// let tbody = document.createElement("TBODY");
+// tbody.setAttribute("ID","dataContainer");
+// let body = document.getElementsByTagName("BODY")[0];
+// body.appendChild(tbody);
+//
+// startExcel();
+// refresh();
+// document.writeln(n(C3) === 6);
+//
+// body.removeChild(tbody);
