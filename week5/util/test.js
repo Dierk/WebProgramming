7413252bad0b3b342b75c16355c9c9472f7e80d9
Dierk Koenig <dierk.koenig@canoo.com>
2018-10-23 21:39:53 +0200

    before week 5

---
+++ week5/util/test.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+
+// test result report
+// report :: String, [Bool] -> DOM ()
+function report(origin, ok) {
+    const extend = 20;
+    if ( ok.every( elem => elem) ) {
+        document.writeln(" "+ padLeft(ok.length, 3) +" tests in " + padRight(origin, extend) + " ok.");
+        return;
+    }
+    let reportLine = "    Failing tests in " + padRight(origin, extend);
+    bar(reportLine.length);
+    document.writeln("|" + reportLine+ "|");
+    for (let i = 0; i < ok.length; i++) {
+        if( ! ok[i]) {
+            document.writeln("|    Test #"+ padLeft(i, 3) +" failed                     |");
+        }
+    }
+    bar(reportLine.length);
+}
+
+function bar(extend) {
+    document.writeln("+" + "-".repeat(extend) + "+");
+}
+
+// padRight :: String, Int -> String
+function padRight(str, extend) {
+    return "" + str + fill(str, extend);
+}
+
+function padLeft(str, extend) {
+    return "" + fill(str, extend) + str;
+}
+
+function fill(str, extend) {
+    const len = str.toString().length; // in case str is not a string
+    if (len > extend) {
+        return str;
+    }
+    return " ".repeat(extend - len);
+}
