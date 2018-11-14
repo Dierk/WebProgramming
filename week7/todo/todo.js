d784a08b2b04ee9cf1c55f748d044a42849b78dc
Dierk Koenig <dierk.koenig@canoo.com>
2018-11-14 10:14:55 +0100

    milestone 0.5, tests running

--- week7/todo/todo.js
+++ week7/todo/todo.js
@@ -1,8 +1,37 @@
 
-function startTodo() {
+let todoContainerRef     = null;
+let numberOfTasksRef     = 0;
+let numberOfOpenTasksRef = 0;
 
+function startTodo(startToDoContainer,
+                   startNumberOfTasks,
+                   startNumberOfOpenTasks) {
+
+    todoContainerRef       = startToDoContainer;
+    numberOfOpenTasksRef   = startNumberOfOpenTasks;
+    numberOfTasksRef       = startNumberOfTasks
 }
 
 function addTodo() {
+
+    const inputTD = document.createElement("TD");
+    const input   = document.createElement("INPUT");
+    inputTD.appendChild(input);
+
+    const xTD = document.createElement("TD");
+    xTD.innerText = "X";
+
+    const okTD = document.createElement("TD");
+    okTD.innerText = "OK";
+
+    const tr = document.createElement("TR");
+
+    tr.appendChild(inputTD);
+    tr.appendChild(xTD);
+    tr.appendChild(okTD);
+
+    todoContainerRef.appendChild(tr);
+    numberOfTasksRef.innerText     = Number(numberOfTasksRef.innerText) + 1;
+    numberOfOpenTasksRef.innerText = Number(numberOfOpenTasksRef.innerText) + 1;
     
 }
--- week7/todo/todoTest.js
+++ week7/todo/todoTest.js
@@ -1,3 +1,31 @@
 // requires todo.js
 // requires /util/test.js
 
+test("todo-setup", assert => {
+    // initially, I should have 0 tasks and 0 open tasks
+
+    const todoContainer = document.createElement("TBODY");
+
+    const numberOfOpenTasks = document.createElement("SPAN");
+    numberOfOpenTasks.innerText = "0";
+
+    const numberOfTasks = document.createElement("SPAN");
+    numberOfTasks.innerText = "0";
+
+    startTodo(todoContainer,numberOfTasks,numberOfOpenTasks);
+
+    assert.true(! todoContainer.hasChildNodes());
+    assert.is(numberOfOpenTasks.innerText, "0");
+    assert.is(numberOfTasks.innerText, "0");
+
+    // todo: when I click on "add" I should have 1 task and 1 open task
+
+    addTodo();
+
+    assert.is(todoContainer.childNodes.length, 1);
+    assert.is(todoContainer.childNodes[0].childNodes[2].innerText, "OK");
+
+    assert.is(numberOfOpenTasks.innerText, "1");
+    assert.is(numberOfTasks.innerText, "1");
+
+});
