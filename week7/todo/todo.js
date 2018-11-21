740c3aa411fbf25dcd05e95d5206c6dddf17d021
Dierk Koenig <dierk.koenig@canoo.com>
2018-11-21 10:00:23 +0100

    week 9, observable number of (open) tasks

--- week7/todo/todo.js
+++ week7/todo/todo.js
@@ -1,14 +1,22 @@
 
-let todoContainerRef     = null;
-let numberOfTasksRef     = 0;
-let numberOfOpenTasksRef = 0;
+let todoContainerRef      = null;
+
+const numberOfTasksObs     = Observable(0);
+const numberOfOpenTasksObs = Observable(0);
+
 
 function startTodo(startToDoContainer,
                    startNumberOfTasks,
                    startNumberOfOpenTasks) {
     todoContainerRef       = startToDoContainer;
-    numberOfOpenTasksRef   = startNumberOfOpenTasks;
-    numberOfTasksRef       = startNumberOfTasks
+
+    numberOfTasksObs.onChange( newValue =>
+        startNumberOfTasks.innerText = newValue
+    );
+    numberOfOpenTasksObs.onChange( newValue =>
+        startNumberOfOpenTasks.innerText = newValue
+    );
+
 }
 
 function addTodo() {
@@ -22,15 +30,16 @@ function addTodo() {
     okTD.innerText = "OK";
     okTD.onclick = evt => {
         okTD.innerText = "Done";
-        numberOfOpenTasksRef.innerText = Number(numberOfOpenTasksRef.innerText) - 1;
+        numberOfOpenTasksObs.setValue( numberOfOpenTasksObs.getValue() - 1 );
+        okTD.onclick = undefined;
     };
 
     const xTD = document.createElement("TD");
     xTD.innerText = "X";
     xTD.onclick = evt => {
-        numberOfTasksRef.innerText = Number(numberOfTasksRef.innerText) - 1;
+        numberOfTasksObs.setValue( numberOfTasksObs.getValue() - 1 );
         if (okTD.innerText === "OK") { // an open task was deleted
-            numberOfOpenTasksRef.innerText = Math.max(0, Number(numberOfOpenTasksRef.innerText) - 1);
+            numberOfOpenTasksObs.setValue(Math.max(0, numberOfOpenTasksObs.getValue() - 1));
         }
         todoContainerRef.removeChild(tr);
     };
@@ -40,7 +49,8 @@ function addTodo() {
     tr.appendChild(okTD);
 
     todoContainerRef.appendChild(tr);
-    numberOfTasksRef.innerText     = Number(numberOfTasksRef.innerText) + 1;
-    numberOfOpenTasksRef.innerText = Number(numberOfOpenTasksRef.innerText) + 1;
+
+    numberOfTasksObs.setValue(numberOfTasksObs.getValue() + 1);
+    numberOfOpenTasksObs.setValue(numberOfOpenTasksObs.getValue() + 1);
     
 }
