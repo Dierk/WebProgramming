5418ee2f197d825dabef6517892fed2f68d96901
Dierk Koenig <dierk.koenig@canoo.com>
2019-03-17 20:16:07 +0100

    before week 5

---
+++ week5/plotter/plotter.js
@@ -0,0 +1,43 @@
+
+const minX =  0;
+const maxX =  6;
+const minY = -1;
+const maxY =  1;
+
+function start() {
+    const userFunction = document.getElementById('user_function');
+    const canvas       = document.getElementById('canvas');
+    const context      = canvas.getContext("2d");
+
+    // todo: how to display?
+
+}
+
+function display(context, f) {
+    // clear
+    context.fillStyle = "papayawhip";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    // draw the function plot
+    normx = normalizeX(canvas.width);
+    normy = normalizeY(canvas.height);
+
+    context.fillStyle = "black";
+    context.beginPath();
+    context.moveTo(normx(minX), normy(f(minX)));
+
+    const stride = (maxX - minX) / 100; // 100 Stützstellen
+    for (let x = minX; x <= maxX; x += stride) {
+        context.lineTo(normx(x), normy(f(x)));
+        context.stroke();
+    }
+}
+
+const normalizeY = height => y => {
+    let scaleFactor = height / (maxY - minY);
+    return height - (y - minY) * scaleFactor;
+};
+
+const normalizeX = width => x => {
+    let scaleFactor = width / (maxX - minX);
+    return ( x - minX) * scaleFactor;
+};
